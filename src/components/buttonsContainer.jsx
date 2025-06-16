de7e70c7cf4bd0d3955cfb262ed3cf79dbde7d5f
Sahil Kamble <syk2448@gmail.com>
2025-06-16 16:52:21 +0530

    some style changes

--- src/components/buttonsContainer.jsx
+++ src/components/buttonsContainer.jsx
@@ -1,35 +1,33 @@
+import { useDebugValue } from "react";
 import styles from "./buttonsContainer.module.css";
 const ButtonsContainer = ({ onButtonClick }) => {
   const buttonNames = [
-    "C",
-    "1",
-    "2",
-    "+",
-    "3",
-    "4",
-    "-",
-    "5",
-    "6",
-    "*",
-    "7",
-    "8",
-    "/",
-    "=",
-    "9",
-    "0",
-    ".",
+    ["C", "1", "2"],
+    ["+", "3", "4"],
+    ["-", "5", "6"],
+    ["*", "7", "8"],
+
+    ["/", "=", "9"],
+    ["0", "."],
   ];
   return (
     <div className={styles.buttonsContainer}>
-      {buttonNames.map((btn) => (
-        <button className={styles.button} onClick={() => onButtonClick(btn)}>
-          {btn}
-        </button>
+      {buttonNames.map((btns, idx) => (
+        <div key={idx} className={styles.btnContWrapper}>
+          {btns.map((btn, idx) => {
+            return (
+              <button
+                key={idx}
+                style={{ cursor: "pointer" }}
+                className={styles.button}
+                onClick={() => onButtonClick(btn)}
+              >
+                <h4>{btn}</h4>
+              </button>
+            );
+          })}
+        </div>
       ))}
-
-      {/* <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>3</button> */}
     </div>
   );
 };
